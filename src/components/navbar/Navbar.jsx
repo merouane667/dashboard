9a0b831558f4e3c74b83cc8e28ed945786b4f346
merouane hany <merouane.hany@ynov.com>
2023-06-23 15:57:51 +0100

    version 3.0

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -34,14 +34,6 @@ const Navbar = ({ onSidebarToggle }) => {
             <FullscreenExitOutlinedIcon className="icon" />
           </div>
           <div className="item">
-            <NotificationsNoneOutlinedIcon className="icon" />
-            <div className="counter">1</div>
-          </div>
-          <div className="item">
-            <ChatBubbleOutlineOutlinedIcon className="icon" />
-            <div className="counter">2</div>
-          </div>
-          <div className="item">
             <ListOutlinedIcon className="icon" />
           </div>
           <div className="item ">
